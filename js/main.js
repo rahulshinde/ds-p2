e0a18c71d1a3dbbcd85da0ded5664ef068289e8d
Rahul Shinde <rshinde@mica.edu>
2016-10-12 16:06:56 -0400

    function for right

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var w = window.innerWidth,
     left_detector1 = w * 0.4;
     left_detector2 = w * 0.1;
+    right_detector1 = w * 0.6;
+    right_detector2 = w * 0.9;
 
 $(document).ready(function () {
     document.onmousemove = trackMouseMovement;
@@ -8,14 +10,22 @@ $(document).ready(function () {
 
 trackMouseMovement = function() {
     if ($('.left_tease').hasClass('show')) {
-        removeClass();
+        removeClassL();
     }
     else {
-        addClass();
+        addClassL();
     }
+
+    if ($('.right_tease').hasClass('show')) {
+        removeClassR();
+    }
+    else {
+        addClassR();
+    }
+
 }
 
-addClass = function() {
+addClassL = function() {
     if (event.clientX < left_detector1) {
         $('.left_tease').addClass('show');
     }
@@ -25,13 +35,36 @@ addClass = function() {
     }
 }
 
-removeClass = function() {
+removeClassL = function() {
     if ((event.clientX) < left_detector2) {
         $('.left').addClass('show');
     }
     else if (event.clientX >= left_detector1) {
         $('.left_tease').removeClass('show');
         $('.left').removeClass('show');
+    }
+    else {
+        return;
+    }
+}
+
+addClassR = function() {
+    if (event.clientX > right_detector1) {
+        $('.right_tease').addClass('show');
+    }
+
+    else {
+        return;
+    }
+}
+
+removeClassR = function() {
+    if ((event.clientX) > right_detector2) {
+        $('.right').addClass('show');
+    }
+    else if (event.clientX <= right_detector1) {
+        $('.right_tease').removeClass('show');
+        $('.right').removeClass('show');
     }
     else {
         return;
